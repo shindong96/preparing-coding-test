25e3ee8c0e1c63cdf7c4df3f3cececb9b2178cee
shindong96 <2006peter@naver.com>
2023-03-30 22:11:44 +0900

    feat: 2번구현

--- src/main/java/simulation/CleaningRobot.java
+++ src/main/java/simulation/CleaningRobot.java
@@ -1,2 +1,34 @@
-package simulation;public class CleaningRobot {
+package simulation;
+
+// 2번 청소
+
+public class CleaningRobot {
+
+    private static final int[] D_X = {1, 0, -1, 0};
+    private static final int[] D_Y = {0, 1, 0, -1};
+
+    public int[] solution(int[][] board, int k) {
+        int[] answer = new int[2];
+
+        int size = board.length;
+
+        int directionIdx = 0;
+
+        for (int i = 0; i < k; i++) {
+            final int nextY = answer[0] + D_Y[directionIdx];
+            final int nextX = answer[1] + D_X[directionIdx];
+
+            if (nextY == size || nextX == size ||
+                    nextX < 0 || nextY < 0 ||
+                    board[nextY][nextX] == 1) {
+                directionIdx++;
+                directionIdx %= 4;
+                continue;
+            }
+
+            answer[0] = nextY;
+            answer[1] = nextX;
+        }
+        return answer;
+    }
 }
--- src/test/java/simulation/CleaningRobotTest.java
+++ src/test/java/simulation/CleaningRobotTest.java
@@ -1,4 +1,46 @@
-import static org.junit.jupiter.api.Assertions.*;
+package simulation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 class CleaningRobotTest {
-  
+
+    @Test
+    void check1() {
+        final CleaningRobot cleaningRobot = new CleaningRobot();
+        int[][] example = {{0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0}};
+        assertThat(cleaningRobot.solution(example, 10))
+                .isEqualTo(new int[]{2, 2});
+    }
+
+    @Test
+    void check2() {
+        final CleaningRobot cleaningRobot = new CleaningRobot();
+        int[][] example = {{0, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1},
+                {1, 1, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0}};
+        assertThat(cleaningRobot.solution(example, 20))
+                .isEqualTo(new int[]{4, 5});
+    }
+
+    @Test
+    void check3() {
+        final CleaningRobot cleaningRobot = new CleaningRobot();
+        int[][] example = {{0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 1},
+                {0, 0, 0, 0, 0}};
+        assertThat(cleaningRobot.solution(example, 25))
+                .isEqualTo(new int[]{0, 1});
+    }
+
 }
