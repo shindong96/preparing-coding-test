cf996453f0f0cae11247d55e4155aa603990221c
shindong96 <2006peter@naver.com>
2023-03-31 19:03:40 +0900

    feat: 1번 오답

--- src/main/java/simulation/ChairNum.java
+++ src/main/java/simulation/ChairNum.java
@@ -1,2 +1,55 @@
-package simulation;public class ChairNum {
+package simulation;
+
+// 4번 문제 
+
+public class ChairNum {
+
+    private static final int[] D_X = {0, 1, 0, -1};
+    private static final int[] D_Y = {1, 0, -1, 0};
+
+    // 1차 시도 오답...
+    public int[] solution(int c, int r, int k) {
+        int[] answer = new int[2];
+
+        int a = 1;
+
+        while (c * r - (c - 2) * (r - 2) <= k && c > 0 && r > 0) {
+            k -= (c * r - (c - 2) * (r - 2));
+            c -= 2;
+            r -= 2;
+            System.out.println(k);
+            a++;
+        }
+
+        if (c <= 0 || r <= 0) {
+            return new int[]{0, 0};
+        }
+
+        int directionIdx = 0;
+
+        while (k - 1 != 0) {
+            k--;
+            //System.out.println(answer[0] + " " + answer[1]);
+
+            int nextX = answer[1] + D_X[directionIdx];
+            int nextY = answer[0] + D_Y[directionIdx];
+
+            if (nextY < 0 || nextX < 0 || nextX >= c || nextY >= r) {
+                directionIdx = (directionIdx + 1) % 4;
+                nextX = answer[1] + D_X[directionIdx];
+                nextY = answer[0] + D_Y[directionIdx];
+            }
+            answer[1] = nextX;
+            answer[0] = nextY;
+
+        }
+
+        //System.out.println(r);
+        answer[0] = r - 1 - answer[0] + a;
+        answer[1] += a;
+        //System.out.println(answer[0] + " " + answer[1]);
+
+        return new int[]{answer[1], answer[0]};
+    }
+
 }
--- src/test/java/simulation/ChairNumTest.java
+++ src/test/java/simulation/ChairNumTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package simulation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+
 class ChairNumTest {
-  
+
+    @Test
+    void check() {
+        final ChairNum chairNum = new ChairNum();
+
+        assertAll(
+                () -> assertThat(chairNum.solution(6, 5, 12)).isEqualTo(new int[]{6, 3}),
+                () -> assertThat(chairNum.solution(6, 5, 20)).isEqualTo(new int[]{2, 3}),
+                () -> assertThat(chairNum.solution(6, 5, 30)).isEqualTo(new int[]{4, 3}),
+                () -> assertThat(chairNum.solution(6, 5, 31)).isEqualTo(new int[]{0, 0})
+        );
+    }
+
 }
