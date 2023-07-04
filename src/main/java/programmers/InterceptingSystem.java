d8df7e7e481bf62ed8cef256239225a1e9044661
shindong96 <2006peter@naver.com>
2023-07-04 16:30:28 +0900

    feat: 메인 없앰

--- src/main/java/programmers/InterceptingSystem.java
+++ src/main/java/programmers/InterceptingSystem.java
@@ -5,16 +5,6 @@ import java.util.Comparator;
 
 public class InterceptingSystem {
 
-    public static void main(String[] args) {
-        final InterceptingSystem interceptingSystem = new InterceptingSystem();
-        int[][] ints = new int[3][];
-        ints[0] = new int[]{1, 4};
-        ints[1] = new int[]{1, 5};
-        ints[2] = new int[]{1, 3};
-        interceptingSystem.solution(ints);
-        System.out.println();
-    }
-
     public int solution(int[][] targets) {
         int end = 0;
         int answer = 0;
